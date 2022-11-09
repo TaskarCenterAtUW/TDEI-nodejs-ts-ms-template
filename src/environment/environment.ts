a0ad5725d24892a3fb507a37a899aee0f3fcd14f
Naresh Kumar D <susrisha@gmail.com>
2022-11-09 10:32:55 +0530

    Update environment.ts
    Removed the keys for connection

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -5,9 +5,9 @@
  */
 export const environment = {
     connections:{
-        serviceBus: "Endpoint=sb://tdei-sample.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=4UNDrVpThcnbqWlGFFQEcivuPlvMMWcSHwbyHgEv+rg=",
-        blobStorage:"DefaultEndpointsProtocol=https;AccountName=tdeisamplestorage;AccountKey=l9JSJCGq9NrXqRVKApSk1wwV27aWaOVuxeY0NWOZz2svIlJzyncr3UFTzLoAanFbmJIeb2WmwIcS+AStj5gELg==;EndpointSuffix=core.windows.net",
-        appInsights:"InstrumentationKey=f98ba6d5-58e1-4267-827e-ccac68caf50f;IngestionEndpoint=https://westus2-2.in.applicationinsights.azure.com/;LiveEndpoint=https://westus2.livediagnostics.monitor.azure.com/"
+        serviceBus: "XXXX",
+        blobStorage:"XXXX",
+        appInsights:"XXXX"
     },
     queueName:"tdei-poc-queue",
     blobContainerName:"tdei-storage-test",
